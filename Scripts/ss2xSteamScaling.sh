dcd2ae4567800ff66bde85e4cfff1a8aebd2c1ee
Dathide <47128084+Dathide@users.noreply.github.com>
2021-08-04 17:10:29 -0700

    Update ss2xSteamScaling.sh

--- Scripts/ss2xSteamScaling.sh
+++ Scripts/ss2xSteamScaling.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cp /usr/share/applications/steam.desktop /home/$USER/.local/share/applications/
 sed -i 's#/usr/bin/steam-runtime %U#GDK_SCALE=2 steam#' /home/$USER/.local/share/applications/steam.desktop
+# Reload .desktop files
 kbuildsycoca5
