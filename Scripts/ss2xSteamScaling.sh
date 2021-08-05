93b8aee7f24c72691c90932dcc22d55fd20f2236
Dathide <47128084+Dathide@users.noreply.github.com>
2021-08-04 17:08:48 -0700

    Add files via upload

---
+++ Scripts/ss2xSteamScaling.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp /usr/share/applications/steam.desktop /home/$USER/.local/share/applications/
+sed -i 's#/usr/bin/steam-runtime %U#GDK_SCALE=2 steam#' /home/$USER/.local/share/applications/steam.desktop
+kbuildsycoca5
