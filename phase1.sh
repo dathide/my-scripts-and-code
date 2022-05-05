263f78ddc711c28f32ccae4e80346ca21c7ca79a
dathide <47128084+dathide@users.noreply.github.com>
2022-05-05 13:37:10 -0700

    Add files via upload

--- phase1.sh
+++ phase1.sh
@@ -1,51 +1,28 @@
 #!/bin/bash
 cd archstuff
-sleep 0.5
 sed -i 's/#Color/Color/' /etc/pacman.conf
-sleep 0.5
 sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
-sleep 0.5
 loadkeys us
-sleep 0.5
 if [[ ! -d "/sys/firmware/efi/efivars" ]] ; then
     echo 'efivars directory is not there, aborting.'
     exit
 fi
-sleep 0.5
 timedatectl set-ntp true
-sleep 0.5
 cryptsetup open /dev/disk/by-label/ARCHBTRFS1 crypt1
-sleep 0.5
 mkfs.btrfs -f /dev/mapper/crypt1
-sleep 0.5
 mount /dev/mapper/crypt1 /mnt
-sleep 0.5
 btrfs subvolume create /mnt/r1
-sleep 0.5
 btrfs subvolume create /mnt/r1home
-sleep 0.5
 btrfs subvolume create /mnt/r1var
-sleep 0.5
 btrfs subvolume create /mnt/r1snapshots
-sleep 0.5
 umount /mnt
-sleep 0.5
 mount -o noatime,compress-force=zstd:3,subvol=r1 /dev/mapper/crypt1 /mnt
-sleep 0.5
 mount --mkdir -o noatime,compress-force=zstd:3,subvol=r1home /dev/mapper/crypt1 /mnt/home
-sleep 0.5
 mount --mkdir -o noatime,compress-force=zstd:3,subvol=r1var /dev/mapper/crypt1 /mnt/var
-sleep 0.5
 mount --mkdir -o noatime,compress-force=zstd:3,subvol=r1snapshots /dev/mapper/crypt1 /mnt/snapshots
-sleep 0.5
 mount --mkdir /dev/disk/by-label/ARCHBOOT1 /mnt/boot
-sleep 0.5
 pacstrap /mnt base linux linux-firmware btrfs-progs dosfstools exfatprogs f2fs-tools e2fsprogs jfsutils nilfs-utils ntfs-3g reiserfsprogs udftools xfsprogs nano vim neovim man-db man-pages texinfo amd-ucode efifs grub efibootmgr cryptsetup plasma-desktop sddm zsh sudo firefox kitty
-sleep 0.5
 genfstab -U /mnt >> /mnt/etc/fstab
-sleep 0.5
 cp phase2.sh /mnt/home
-sleep 0.5
 arch-chroot /mnt
-sleep 0.5
 # then need to /bin/bash /home/phase2.sh
--- phase2.sh
+++ phase2.sh
@@ -1,49 +1,30 @@
 ln -sf /usr/share/zoneinfo/America/Phoenix /etc/localtime
-sleep 0.5
 hwclock --systohc
-sleep 0.5
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-sleep 0.5
 locale-gen
-sleep 0.5
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
-sleep 0.5
 echo "KEYMAP=us" > /etc/vconsole.conf
-sleep 0.5
 echo "archlinux" > /etc/hostname
-sleep 0.5
 echo "#<ip-address> <hostname.domain.org> <hostname>" > /etc/hosts
-sleep 0.5
 echo "127.0.0.1 archlinux.localdomain archlinux" >> /etc/hosts
-sleep 0.5
 echo "::1 localhost.localdomain localhost" >> /etc/hosts
-sleep 0.5
 systemctl enable systemd-networkd.service
-sleep 0.5
 mkdir /etc/modules-load.d
-sleep 0.5
 echo "dm_crypt" > /etc/modules-load.d/dm_crypt.conf
-sleep 0.5
 # After lines starting with MODULES, add line
 # MODULES=(btrfs)
 # From beginning to first line starting with MODULES, replace MODULES with #MODULES
 sed -i '0,/^\MODULES/ s//#MODULES/' /etc/mkinitcpio.conf
-sleep 0.5
 
-sleep 0.5
 # The order of hooks is important
 # Order used from https://wiki.archlinux.org/title/Dm-crypt/Encrypting_an_entire_system#Configuring_mkinitcpio
 # From beginning to first line starting with HOOKS, add line
 # HOOKS=(base udev autodetect keyboard keymap consolefont modconf block encrypt filesystems fsck)
 # From beginning to first line starting with HOOKS, replace HOOKS with #HOOKS
 sed -i '0,/^\HOOKS/ s//#HOOKS/' /etc/mkinitcpio.conf
-sleep 0.5
 
-sleep 0.5
 mkinitcpio -P
-sleep 0.5
 passwd
-sleep 0.5
 
 # Failed systemd-boot attempt. bootctl install didn't create the necessary EFI boot entries.
 : '
@@ -75,22 +56,12 @@ bootctl --esp-path=/efi install
 '
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ARCHLINUX
-sleep 0.5
 sed -i 's@GRUB_CMDLINE_LINUX_DEFAULT="@GRUB_CMDLINE_LINUX_DEFAULT="cryptdevice=UUID=0ed20ab4-ad66-4f70-bc63-fc1dd33dbe1e:crypt1 root=/dev/mapper/crypt1 amdgpu.ppfeaturemask=0xffffffff CONFIG_FW_LOADER_COMPRESS@' /etc/mkinitcpio.conf
-sleep 0.5
 echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
-sleep 0.5
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ARCHLINUX
-sleep 0.5
 grub-mkconfig -o /boot/grub/grub.cfg
-sleep 0.5
 sed -i 's/#Color/Color/' /etc/pacman.conf
-sleep 0.5
 sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
-sleep 0.5
 systemctl enable sddm.service
-sleep 0.5
 useradd -m -G users,wheel,uucp -s /bin/zsh sapien
-sleep 0.5
 passwd sapien
-sleep 0.5
