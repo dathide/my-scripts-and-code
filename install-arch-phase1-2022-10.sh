60cd3ceb92176383f4a7f3fe1a754c8ea3358f14
dathide <47128084+dathide@users.noreply.github.com>
2022-10-15 22:24:07 -0700

    Add files via upload

---
+++ install-arch-phase1-2022-10.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd scripts
+# Comment out any lines that start with ParallelDownloads
+sudo sed -i 's/^ParallelDownloads/#ParallelDownloads/' /etc/pacman.conf
+# Replace first line that starts with #ParallelDownloads with ParallelDownloads = 5
+# Within 0 to first line starting with X, replace line starting with X with Y
+sudo sed -i '0,/^#ParallelDownloads/{s/^#ParallelDownloads.*/ParallelDownloads = 5/}' /etc/pacman.conf
+sudo reflector --verbose --latest 10 --country 'United States' --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+loadkeys en
+timedatectl status
+read -p "Did you set the type of $1 to EFI System?" -n 1 -r
+read -p "Format $1 as boot and $2 as fsroot? " -n 1 -r
+echo #New line
+if [[ $REPLY =~ ^[Yy]$ ]] && [ -d "/sys/firmware/efi/efivars" ]; then
+    mkfs.fat -F 32 $1
+    mkfs.btrfs $2
+    mount $2 /mnt
+    mount --mkdir /dev/$1 /mnt/boot
+    pacstrap -K /mnt base base-devel linux linux-firmware amd-ucode btrfs-progs dosfstools exfatprogs exfat-utils f2fs-tools e2fsprogs jfsutils nilfs-utils ntfs-3g reiserfsprogs udftools xfsprogs iproute2 networkmanager nano vi kitty firefox man-db man-pages texinfo zsh xorg nvidia nvidia-utils nvidia-settings plasma plasma-wayland-session egl-wayland kde-applications sddm sddm-kcm
+    genfstab -U /mnt >> /mnt/etc/fstab
+    cp install-arch-phase2-2022-10.sh /mnt/root
+    arch-chroot /mnt /bin/bash /root/install-arch-phase2-2022-10.sh $1 $2
+fi
---
+++ install-arch-phase2-2022-10.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Comment out any lines that start with ParallelDownloads
+sudo sed -i 's/^ParallelDownloads/#ParallelDownloads/' /etc/pacman.conf
+# Replace first line that starts with #ParallelDownloads with ParallelDownloads = 5
+# Within 0 to first line starting with X, replace line starting with X with Y
+sudo sed -i '0,/^#ParallelDownloads/{s/^#ParallelDownloads.*/ParallelDownloads = 5/}' /etc/pacman.conf
+ln -sf /usr/share/zoneinfo/America/Phoenix /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=en" >> /etc/vconsole.conf
+echo "arch" >> /etc/hostname
+systemctl enable NetworkManager.service
+systemctl enable sddm.service
+passwd
+bootctl --path=/boot install
+echo "title   Arch Linux Oct 2022" > /boot/loader/entries/arch.conf
+echo "linux   /vmlinuz-linux" >> /boot/loader/entries/arch.conf
+echo "initrd  /amd-ucode.img" >> /boot/loader/entries/arch.conf
+echo "initrd  /initramfs-linux.img" >> /boot/loader/entries/arch.conf
+echo "options root=PARTUUID=$(blkid -o value -s UUID $1) rw" >> /boot/loader/entries/arch.conf
+echo "default arch" > /boot/loader/loader.conf
+echo "timeout 4" >> /boot/loader/loader.conf
+echo "console-mode max" >> /boot/loader/loader.conf
+echo "editor no" >> /boot/loader/loader.conf
+bootctl --path=/boot update
+UNAME="sapien"
+useradd -m -G "wheel,adm,ftp,games,http,log,rfkill,sys,systemd-journal,uucp,power,network,vboxusers" -s /bin/zsh $UNAME
+passwd $UNAME
