798dcf70f7049b5826359566ac2e0f987cb6aa92
dathide <47128084+dathide@users.noreply.github.com>
2022-10-15 22:46:35 -0700

    Add files via upload

--- install-arch-phase1-2022-10.sh
+++ install-arch-phase1-2022-10.sh
@@ -15,7 +15,7 @@ if [[ $REPLY =~ ^[Yy]$ ]] && [ -d "/sys/firmware/efi/efivars" ]; then
     mkfs.btrfs $2
     mount $2 /mnt
     mount --mkdir /dev/$1 /mnt/boot
-    pacstrap -K /mnt base base-devel linux linux-firmware amd-ucode btrfs-progs dosfstools exfatprogs exfat-utils f2fs-tools e2fsprogs jfsutils nilfs-utils ntfs-3g reiserfsprogs udftools xfsprogs
+    pacstrap -K /mnt base linux linux-firmware amd-ucode btrfs-progs dosfstools exfatprogs f2fs-tools e2fsprogs jfsutils nilfs-utils ntfs-3g reiserfsprogs udftools xfsprogs iproute2 networkmanager nano vi kitty firefox man-db man-pages texinfo zsh xorg nvidia nvidia-utils nvidia-settings plasma plasma-wayland-session egl-wayland kde-applications sddm sddm-kcm
     genfstab -U /mnt >> /mnt/etc/fstab
     cp install-arch-phase2-2022-10.sh /mnt/root
     arch-chroot /mnt /bin/bash /root/install-arch-phase2-2022-10.sh $1 $2
