a73d7a9e431c8277d152304dd7f96f037606defc
dathide <47128084+dathide@users.noreply.github.com>
2022-10-16 13:13:18 -0700

    Add files via upload

--- install-arch-phase1-2022-10.sh
+++ install-arch-phase1-2022-10.sh
@@ -10,11 +10,15 @@ if [[ $REPLY =~ ^[Yy]$ ]] && [ -d "/sys/firmware/efi/efivars" ]; then
     mkfs.fat -F 32 $1
     fatlabel $1 "BOOT2"
     mkfs.btrfs -f -L "ARCH2" $2
-    mount $2 /mnt
-    mount --mkdir /dev/$1 /mnt/boot
+    mount --mkdir $2 /root/btrfs
+    btrfs subvolume create /root/btrfs/subv_arch
+    umount /root/btrfs
+    mount -o "subvol=subv_arch,rw,noatime,compress-force=zstd:3,noautodefrag" $2 /mnt
+    mount --mkdir $1 /mnt/boot
     pacstrap -K /mnt base linux linux-firmware nano networkmanager amd-ucode btrfs-progs dosfstools exfatprogs f2fs-tools e2fsprogs jfsutils nilfs-utils ntfs-3g reiserfsprogs udftools xfsprogs vi kitty firefox man-db man-pages texinfo zsh xorg nvidia nvidia-utils nvidia-settings plasma plasma-wayland-session egl-wayland sddm sddm-kcm
     genfstab -U /mnt >> /mnt/etc/fstab
     P2="install-arch-phase2-2022-10.sh"
     cp $P2 /mnt/root/$P2
     arch-chroot /mnt /bin/bash /root/$P2 $1 $2
+    umount -R /mnt
 fi
