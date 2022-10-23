54a033a4693775c7c188a203983352e4780894a4
dathide <47128084+dathide@users.noreply.github.com>
2022-10-22 19:08:14 -0700

    Add files via upload

--- install-arch-22a-p1.sh
+++ install-arch-22a-p1.sh
@@ -14,8 +14,8 @@ if [[ $REPLY =~ ^[Yy]$ ]] && [ -d "/sys/firmware/efi/efivars" ]; then
     btrfs subvolume create /root/btrfs/subvol_os1_fsroot
     btrfs subvolume create /root/btrfs/subvol_os1_var_cache_pacman_pkg
     umount /root/btrfs
-    mount -o "subvol=subvol_os1_fsroot,rw,noatime,compress-force=zstd:3,noautodefrag" $2 /mnt
-    mount -m -o "subvol=subvol_os1_var_cache_pacman_pkg,rw,noatime,compress-force=zstd:3,noautodefrag" $2 /mnt/var/cache/pacman/pkg
+    mount -m -o "subvol=subvol_os1_fsroot,rw,noatime,compress-force=zstd:3,noautodefrag" $2 /mnt
+    mount -m -o "subvol=subvol_os1_var_cache_pacman_pkg,rw,noatime,noautodefrag" $2 /mnt/var/cache/pacman/pkg
     mount -m $1 /mnt/boot
     pacstrap -K /mnt base linux linux-firmware nano networkmanager amd-ucode btrfs-progs dosfstools exfatprogs f2fs-tools e2fsprogs jfsutils nilfs-utils ntfs-3g reiserfsprogs udftools xfsprogs vi kitty firefox man-db man-pages texinfo zsh xorg xorg-xwayland nvidia nvidia-utils nvidia-settings plasma plasma-wayland-session egl-wayland sddm sddm-kcm pipewire wireplumber pipewire-pulse
     genfstab -U /mnt >> /mnt/etc/fstab
