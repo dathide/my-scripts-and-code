b708a4a200d9e81a365428c3bcfa428e9401febb
dathide <47128084+dathide@users.noreply.github.com>
2023-06-03 12:14:06 -0700

    Add files via upload

--- use-amt-frame-interpolation.sh
+++ use-amt-frame-interpolation.sh
@@ -30,33 +30,35 @@ if ls ./*.jpg &> /dev/null; then
 
 # Loop until all files are processed
 while [ "$COUNTER" -lt "$ALL_JPG_NUM" ]; do
+  cd "$ALL_FRAMES" || exit
+  # Ensure correct behavior depending on how many jpgs are left
+  REMAINING=$(( ALL_JPG_NUM - COUNTER ))
+  if [ $REMAINING -lt $BATCH_SIZE ]; then
+      # Copy the next batch of files to the destination folder
+      find . -maxdepth 1 -name "*.jpg" | sort | head -n $((COUNTER + BATCH_SIZE)) | tail -n $REMAINING | xargs -I {} cp {} "$AMT/$IN"
+  else
+      # Copy the next batch of files to the destination folder
+      find . -maxdepth 1 -name "*.jpg" | sort | head -n $((COUNTER + BATCH_SIZE)) | tail -n $BATCH_SIZE | xargs -I {} cp {} "$AMT/$IN"
+  fi
 
-    cd "$ALL_FRAMES" || exit
-    # Ensure correct behavior depending on how many jpgs are left
-    REMAINING=$(( ALL_JPG_NUM - COUNTER ))
-    if [ $REMAINING -lt $BATCH_SIZE ]; then
-        # Copy the next batch of files to the destination folder
-        find . -maxdepth 1 -name "*.jpg" | sort | head -n $((COUNTER + BATCH_SIZE)) | tail -n $REMAINING | xargs -I {} cp {} "$AMT/$IN"
-    else
-        # Copy the next batch of files to the destination folder
-        find . -maxdepth 1 -name "*.jpg" | sort | head -n $((COUNTER + BATCH_SIZE)) | tail -n $BATCH_SIZE | xargs -I {} cp {} "$AMT/$IN"
-    fi
+  # Use AMT to interpolate frames and save them in results/2x/samples
+  cd "$AMT" || exit
+  python demos/demo_2x.py -c cfgs/AMT-S.yaml -p pretrained/amt-s.pth --save_images -n 1 -r "$OUT_FPS" -i ./$IN/*.jpg
 
-    # Use AMT to interpolate frames and save them in results/2x/samples
-    cd "$AMT" || exit
-    python demos/demo_2x.py -c cfgs/AMT-S.yaml -p pretrained/amt-s.pth --save_images -n 1 -r "$OUT_FPS" -i ./$IN/*.jpg
-
+  # If this isn't the last batch
+  if [ ! $((COUNTER + BATCH_SIZE)) -gt "$ALL_JPG_NUM" ]; then
     # Delete the last result jpg file so that there aren't duplicates in the final video
     cd "$SAMPLES" || exit
     find "$SAMPLES" -name "*.jpg" | sort | tail -n 1 | xargs rm
+  fi
 
-    # Delete all but the last file in the input-frames folder
-    cd "$AMT/$IN" || exit
-    AMOUNT_OF_INPUT_FRAMES=$(find "$AMT/$IN" -maxdepth 1 -name "*.jpg" | wc -l)
-    find "$AMT/$IN" -name "*.jpg" | sort | head -n $((AMOUNT_OF_INPUT_FRAMES - 1)) | xargs -I {} rm {}
+  # Delete all but the last file in the input-frames folder
+  cd "$AMT/$IN" || exit
+  AMOUNT_OF_INPUT_FRAMES=$(find "$AMT/$IN" -maxdepth 1 -name "*.jpg" | wc -l)
+  find "$AMT/$IN" -name "*.jpg" | sort | head -n $((AMOUNT_OF_INPUT_FRAMES - 1)) | xargs -I {} rm {}
 
-    # Increment the COUNTER by the batch size
-    COUNTER=$((COUNTER + BATCH_SIZE))
+  # Increment the COUNTER by the batch size
+  COUNTER=$((COUNTER + BATCH_SIZE))
 done
 
 # Remove any leftover jpgs
